90c11e8edbbc0ea9fa59545761cc6d7b6b17398c
tikonexdev <pastolektorreapload@gmail.com>
2021-11-04 19:58:14 +0100

    g

---
+++ scripts/script.js
@@ -0,0 +1,11 @@
+let nazwa = prompt("jak masz na imie");
+console.log("hello " + nazwa);
+console.log("dupa");
+const nazka = ("du[a");
+console.log(nazka)
+let osoba = {
+    name:"tiko",
+    age:"2"
+}
+console.log(osoba.age)
+console.log(osoba.age)
