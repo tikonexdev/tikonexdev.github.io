26b5130eeb42feb2665166a6b3a4d8ef48e92aa3
tikonexdev <pastolektorreapload@gmail.com>
2022-02-02 18:25:39 +0100

    dsd

---
+++ php.php
@@ -0,0 +1,16 @@
+<!DOCTYPE <!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    </head>
+    <body>
+    <?php
+echo "dupa"
+
+?>
+    </body>
+</html>
+
+</html>>
